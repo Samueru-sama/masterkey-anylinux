96ff6d8955373efca9fa12eacc01b4ef83c075fd
TheRealYasri <myoung1985@gmail.com>
2026-05-13 19:47:00 -0500

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -eu
 
+# Setup
 VERSION=$(grep -m 1 "version:" source/meson.build | cut -d"'" -f2)
 export VERSION
 export ARCH=$(uname -m)
@@ -11,9 +12,10 @@ export ICON=/usr/share/icons/hicolor/scalable/apps/com.gitlab.guillermop.MasterK
 export DESKTOP=/usr/share/applications/com.gitlab.guillermop.MasterKey.desktop
 
 PY_VER=$(python3 -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')
-
-sudo sed -i "s|pkgdatadir = ['\"].*/share/master-key['\"]|pkgdatadir = os.path.join(os.environ.get('APPDIR', '/'), 'usr/share/master-key')|g" /usr/bin/master-key
-sudo sed -i "2i import os, sys; sys.path.insert(0, os.path.join(os.environ.get('APPDIR', '/'), 'usr/lib/python$PY_VER/site-packages')); os.environ['GSETTINGS_SCHEMA_DIR'] = os.path.join(os.environ.get('APPDIR', '/'), 'usr/share/glib-2.0/schemas')" /usr/bin/master-key
+LIB_DIR="AppDir/usr/lib"
+SITE_PACKAGES="$LIB_DIR/python$PY_VER/site-packages"
+SCHEMA_DIR="AppDir/usr/share/glib-2.0/schemas"
+DATA_DIR="AppDir/usr/share/master-key"
 
 sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
 
@@ -21,30 +23,52 @@ sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
 quick-sharun /usr/bin/master-key
 
 # Additional changes can be done in between here
-SITE_PACKAGES="AppDir/usr/lib/python$PY_VER/site-packages"
-mkdir -p "$SITE_PACKAGES"
-pip install --target="$SITE_PACKAGES" pycryptodome zxcvbn
+mkdir -p "$SITE_PACKAGES" "$LIB_DIR" "$SCHEMA_DIR" "$DATA_DIR"
 
-mkdir -p AppDir/usr/lib
-DEPS="libpwquality.so.1 libsqlcipher.so.0 libtcl8.6.so"
-for LIB in $DEPS; do
-    FILE=$(find /usr/lib -name "$LIB*" | head -n 1)
-    if [ -n "$FILE" ]; then
-        cp -L "$FILE" AppDir/usr/lib/
-        BASE=$(basename "$FILE")
-        if [ "$BASE" != "$LIB" ]; then
-            ln -sf "$BASE" "AppDir/usr/lib/$LIB"
-        fi
-    fi
-done
-
-mkdir -p AppDir/usr/share/master-key
 if [ -d "/usr/share/master-key" ]; then
-    cp -r /usr/share/master-key/* AppDir/usr/share/master-key/
+    cp -r /usr/share/master-key/* "$DATA_DIR/"
 fi
 
-mkdir -p AppDir/usr/share/glib-2.0/schemas/
-cp /usr/share/glib-2.0/schemas/gschemas.compiled AppDir/usr/share/glib-2.0/schemas/
+cp /usr/share/glib-2.0/schemas/com.gitlab.guillermop.MasterKey.gschema.xml "$SCHEMA_DIR/" 2>/dev/null || true
+glib-compile-schemas "$SCHEMA_DIR/"
+
+pip install --target="$SITE_PACKAGES" pycryptodome zxcvbn
+
+DEPS="libpwquality.so.1 libsqlcipher.so.0 libtcl8.6.so"
+for LIB in $DEPS; do
+    FILES=$(find /usr/lib -name "$LIB*" 2>/dev/null)
+    for FILE in $FILES; do
+        if [ -f "$FILE" ]; then
+            cp -L "$FILE" "$LIB_DIR/"
+            BASE=$(basename "$FILE")
+            # Create a symlink to the base library name if it includes a version suffix
+            NAME_ONLY=$(echo "$LIB" | cut -d. -f1-2)
+            if [ "$BASE" != "$NAME_ONLY" ]; then
+                ln -sf "$BASE" "$LIB_DIR/$NAME_ONLY"
+            fi
+        fi
+    done
+done
+
+mv AppDir/bin/master-key AppDir/bin/master-key.real
+
+sed -i "s|pkgdatadir = .*|import os; pkgdatadir = os.path.join(os.environ.get('APPDIR', '/'), 'usr/share/master-key')|g" AppDir/bin/master-key.real
+sed -i "s|localedir = .*|localedir = os.path.join(os.environ.get('APPDIR', '/'), 'usr/share/locale')|g" AppDir/bin/master-key.real
+
+cat << 'EOF' > AppDir/bin/master-key
+#!/bin/sh
+HERE="$(dirname "$(readlink -f "$0")")"
+export APPDIR="${HERE%/*}"
+PY_VER=$(python3 -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')
+
+export LD_LIBRARY_PATH="$APPDIR/usr/lib:$LD_LIBRARY_PATH"
+export PYTHONPATH="$APPDIR/usr/lib/python$PY_VER/site-packages:$PYTHONPATH"
+export GSETTINGS_SCHEMA_DIR="$APPDIR/usr/share/glib-2.0/schemas"
+
+exec python3 "$HERE/master-key.real" "$@"
+EOF
+
+chmod +x AppDir/bin/master-key
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
